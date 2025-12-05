894f1b686aa470adb3e57158c60f980f44ed4fbc
sharath <sharathsree916@gmail.com>
2025-12-05 12:54:46 +0530

    testing

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,34 @@ import user from "../models/User.js"
 
 const route =express.Router();
 
+route.post("/login",async (req,res)=>{
+    try{
+        const {email,password}=req.body
+        if(!email || !password){
+            return res.status(400).json({message:"email or password required"})
+        }
+        const user = await user.findOne({email})
+        if (!user){return res.status(400).json({message:"invalid email or password"})}
+        const token =JWT.sign(
+            {id:user._id},
+            process.env.JWT_SECRET,
+            {expiresIn:"12h"}
+        )
+        res.json({
+            message:"login successful",
+            token,
+            user:{
+                id:user._id,
+                name:user.name,
+                email:user.email
+            }
+        })
+    }catch(err){
+        console.error("login error",err)
+        res.status(500).json({message:"server error"})
+    }
+})
+
 route.post("/register",async (req,res)=>{
     const {name,email,password}=req.body
 
